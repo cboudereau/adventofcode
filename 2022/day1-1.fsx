8b649187fa1df3d3bd6c25429460c34d2ba22827
Clément Boudereau <cboudereau@gmail.com>
2022-12-06 17:13:29 +0100

    add 2022 day2-1

--- 2022/day1-1.fsx
+++ 2022/day1-1.fsx
@@ -6,6 +6,15 @@ module Int32 =
 
 let (|Int32|_|) = Int32.tryParse
 
+let parse input =
+    input
+    |> Array.fold (fun s x ->
+        match s, x with
+        | [], Int32 x -> [x]
+        | h::t, Int32 x -> (h + x) :: t
+        | s, _ -> 0::s
+    ) List.empty    
+
 let day11 (input:string[]) = 
     input
     |> Array.fold (fun s x ->
@@ -33,8 +42,14 @@ let input = """1000
 
 input |> day11 = 24000
 
-let readFile filePath = System.IO.File.ReadAllLines(filePath)
+let readFile filePath = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + filePath)
 
-let fullInput = __SOURCE_DIRECTORY__ + "/day1-1.txt" |> readFile
+let fullInput = "/day1-1.txt" |> readFile
 
 fullInput |> day11 = 68292
+
+let day12 = parse >> List.sortDescending >> List.truncate 3 >> List.sum
+
+day12 input = 45000
+
+"/day1-2.txt" |> readFile |> day12
