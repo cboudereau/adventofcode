665b4bf3dbb0c63565e4f0677f65d74d833ae6c1
Clément Boudereau <cboudereau@gmail.com>
2023-06-23 11:37:32 +0200

    clean

--- 2022/day10.fsx
+++ 2022/day10.fsx
@@ -30,37 +30,36 @@ Command.parse "addx -2" |> Test.assertEq "addx command parsing" (2, -2)
 
 let readFile filePath = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + filePath) 
 
-let strength newCycles newRegister cycles register = 
-    let strength newCycles newRegister cycles register threshold = 
+let strength newCycles cycles register = 
+    let strength newCycles cycles register threshold = 
         if cycles < threshold && newCycles >= threshold then
-            let register = if newCycles < threshold then newRegister else register
             register * threshold
         else 0
     
-    [20;60;100;140;180;220] |> List.tryPick (fun t -> let s = strength newCycles newRegister cycles register t in if s > 0 then Some s else None) |> Option.defaultValue 0
+    [220;180;140;100;60;20] |> List.tryPick (fun t -> let s = strength newCycles cycles register t in if s > 0 then Some s else None) |> Option.defaultValue 0
 
-strength 20 2 18 1 |> Test.assertEq "20 strength test" 20
-strength 21 3 19 2 |> Test.assertEq "21 strength test" 40
-strength 30 2 28 1 |> Test.assertEq "no strength test" 0
+strength 20 18 1 |> Test.assertEq "20 strength test" 20
+strength 21 19 2 |> Test.assertEq "21 strength test" 40
+strength 30 28 1 |> Test.assertEq "no strength test" 0
 
-strength 60 3 58 1 |> Test.assertEq "60 strength test" 60
-strength 61 3 59 2 |> Test.assertEq "60 strength test" 120
-strength 62 3 60 2 |> Test.assertEq "no strength test" 0
+strength 60 58 1 |> Test.assertEq "60 strength test" 60
+strength 61 59 2 |> Test.assertEq "60 strength test" 120
+strength 62 60 2 |> Test.assertEq "no strength test" 0
 
-strength 220 3 218 1 |> Test.assertEq "220 strength test" 220
-strength 221 3 219 2 |> Test.assertEq "221 strength test" 440
-strength 300 3 299 3 |> Test.assertEq "no strength test" 0
+strength 220 218 1 |> Test.assertEq "220 strength test" 220
+strength 221 219 2 |> Test.assertEq "221 strength test" 440
+strength 300 299 3 |> Test.assertEq "no strength test" 0
 
 let part1 = 
     readFile
-    >> Array.fold (fun (sum, (cycles, register)) input -> 
+    >> Array.fold (fun (sum, (totalCycles, register)) input -> 
 
-        let (c, x) = Command.parse input
+        let (cycles, x) = Command.parse input
 
-        let newCycles = cycles + c
+        let newCycles = totalCycles + cycles
         let newRegister = register + x
-        let strength = strength newCycles newRegister cycles register
-        if strength > 0 then printfn "strength: %i cycles: %i newCycles: %i register: %i newRegister: %i" strength cycles newCycles register newRegister
+        let strength = strength newCycles totalCycles register
+        if strength > 0 then printfn "strength: %i cycles: %i newCycles: %i register: %i newRegister: %i" strength totalCycles newCycles register newRegister
         let newSum = sum + strength
         (newSum, (newCycles, newRegister))
     ) (0,(0,1))
