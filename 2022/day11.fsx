bea825bf0f901d21b93afa63099a208b2171fcaa
Clément Boudereau <cboudereau@gmail.com>
2023-06-27 10:57:36 +0200

    2022: day11 part2

--- 2022/day11.fsx
+++ 2022/day11.fsx
@@ -9,8 +9,8 @@ let (|Regex|_|) options pattern input =
     if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
     else None
 
-let (|Int32|_|) x = 
-    match System.Int32.TryParse(x:string) with
+let (|Int64|_|) x = 
+    match System.Int64.TryParse(x:string) with
     | true, v -> Some v
     | false, _ -> None
 
@@ -18,10 +18,10 @@ type MonkeyId = int
 
 type Monkey = {
     MonkeyId: int
-    Items: System.Collections.Generic.Queue<int>
-    Operation: int -> int
-    Test: int -> MonkeyId * int
-    mutable TotalInspections: int
+    Items: System.Collections.Generic.Queue<int64>
+    Operation: int64 -> int64
+    Test: int64 -> MonkeyId
+    mutable TotalInspections: int64
 }
 
 let parse = 
@@ -29,23 +29,23 @@ let parse =
     | Regex System.Text.RegularExpressions.RegexOptions.Multiline """Monkey (\d+):\s*Starting items: ([\d,\s]+)\s*Operation: new = old ([\+\*]) ([^\s]+)\s*Test: divisible by (\d+)\s*If true: throw to monkey (\d+)\s*If false: throw to monkey (\d+)\s*""" [monkeyId; items; sign; v; divisible; monkeyTrue; monkeyFalse] -> 
         { Monkey.MonkeyId = int monkeyId
           TotalInspections = 0
-          Items = items.Split(',', System.StringSplitOptions.RemoveEmptyEntries) |> Array.map(fun x -> int x) |> System.Collections.Generic.Queue
+          Items = items.Split(',', System.StringSplitOptions.RemoveEmptyEntries) |> Array.map(fun x -> int64 x) |> System.Collections.Generic.Queue
           Operation = 
             let op = match sign with "*" -> (*) | "+" -> (+) | other -> failwithf "unexpected op '%s'" other
             match v with
-            | Int32 v -> op v
+            | Int64 v -> op v
             | "old" -> fun x -> op x x
             | other -> failwithf "unexpected value op '%s'" other
           Test = 
             match divisible, monkeyTrue, monkeyFalse with
-            | Int32 d, Int32 mTrue, Int32 mFalse -> 
+            | Int64 d, Int64 mTrue, Int64 mFalse -> 
                 fun x -> 
                     if x % d = 0 then 
-                        printfn "  Current worry (%i) level is divisible by %i." x d
-                        mTrue, x 
+                        // printfn "  Current worry (%i) level is divisible by %i." x d
+                        int mTrue
                     else 
-                        printfn "  Current worry level (%i) is not divisible by %i." x d
-                        mFalse, x
+                        // printfn "  Current worry level (%i) is not divisible by %i." x d
+                        int mFalse
             | other -> failwithf "unexpected divisible '%A'" other
         }
     | other -> failwithf "unexpected %s" other
@@ -62,32 +62,30 @@ let readFile filePath = System.IO.Path.Combine(__SOURCE_DIRECTORY__, filePath) |
 
 let split x = (x:string).Split("\r\n\r\n", System.StringSplitOptions.RemoveEmptyEntries)
 
-let turn n (monkeys:Monkey[]) : Monkey[] = 
-    [1..n]
-    |> List.fold(fun monkeys _ -> 
-        monkeys
-        |> Array.fold (fun monkeys m -> 
-            let rec turn monkeys (m:Monkey) = 
-              match m.Items.TryDequeue() with
-              | true, item -> 
-                m.TotalInspections <- m.TotalInspections + 1
-                printfn " Monkey inspects an item with a worry level of %i" item
-                let item = m.Operation item
-                let item = item / 3
-                printfn "  Monkey gets bored with item. Worry level is divided by 3 to %i." item
-                let (mId, item) = m.Test item
-                let target = monkeys |> Array.find(fun x -> x.MonkeyId = mId)
-                printfn "  Item with worry level %i is thrown to monkey %i" item mId
-                target.Items.Enqueue(item)
-                turn monkeys m
-              | false, _ -> monkeys            
-            printfn "Monkey %i:" m.MonkeyId
-            turn monkeys m
-        ) monkeys
-    ) monkeys
-
-
 let part1 input = 
+    let turn n (monkeys:Monkey[]) : Monkey[] = 
+        [1..n]
+        |> List.fold(fun monkeys _ -> 
+            monkeys
+            |> Array.fold (fun monkeys m -> 
+                let rec turn monkeys (m:Monkey) = 
+                    match m.Items.TryDequeue() with
+                    | true, item -> 
+                        m.TotalInspections <- m.TotalInspections + 1L
+                        printfn " Monkey inspects an item with a worry level of %i" item
+                        let item = m.Operation item
+                        let item = item / 3L
+                        printfn "  Monkey gets bored with item. Worry level is divided by 3 to %i." item
+                        let mId = m.Test item
+                        let target = monkeys |> Array.find(fun x -> x.MonkeyId = mId)
+                        printfn "  Item with worry level %i is thrown to monkey %i" item mId
+                        target.Items.Enqueue(item)
+                        turn monkeys m
+                    | false, _ -> monkeys            
+                printfn "Monkey %i:" m.MonkeyId
+                turn monkeys m
+            ) monkeys
+        ) monkeys
     input
     |> split
     |> Array.map parse
@@ -95,7 +93,47 @@ let part1 input =
     |> Array.sortByDescending (fun x -> x.TotalInspections)
     |> Array.take 2
     |> Array.map (fun x -> x.TotalInspections)
-    |> Array.fold (*) 1
+    |> Array.fold (*) 1L
 
 "./day11.example.txt" |> readFile |> part1 |> Test.assertEq "example" 10605
 "./day11.txt" |> readFile |> part1 |> Test.assertEq "example" 55944
+
+let part2 n input = 
+    let checks = [1;20;1000;2000;3000;4000;5000;6000;7000;8000;9000;10000]
+    let turn n (monkeys:Monkey[]) : Monkey[] = 
+        [1..n]
+        |> List.fold(fun monkeys i -> 
+            let monkeys = 
+                monkeys
+                |> Array.fold (fun monkeys m -> 
+                    let rec turn monkeys (m:Monkey) = 
+                        match m.Items.TryDequeue() with
+                        | true, item -> 
+                            m.TotalInspections <- m.TotalInspections + 1L
+                            let item = m.Operation item
+                            let item = item % (7L * 19L * 13L * 3L * 2L * 11L * 17L * 5L)
+                            let mId  = m.Test item
+                            let target = monkeys |> Array.find(fun x -> x.MonkeyId = mId)
+                            target.Items.Enqueue(item)
+                            turn monkeys m
+                        | false, _ -> monkeys            
+                    turn monkeys m
+                ) monkeys
+            if checks |> List.contains i then 
+                printfn "== After round %i ==" i
+                monkeys |> Array.iter(fun m -> printfn "Monkey %i inspected items %i times." m.MonkeyId m.TotalInspections)
+                printfn ""
+            monkeys
+        ) monkeys
+    input
+    |> turn n
+    |> Array.sortByDescending (fun x -> x.TotalInspections)
+    |> Array.take 2
+    |> Array.map (fun x -> x.TotalInspections)
+    |> Array.fold (*) 1L
+
+"./day11.example.txt" |> readFile |> split |> Array.map parse |> part2 10000 
+|> Test.assertEq "part2 example" 2567194800L
+
+"./day11.txt" |> readFile |> split |> Array.map parse |> part2 10000
+|> Test.assertEq "part2" 15117269860L
