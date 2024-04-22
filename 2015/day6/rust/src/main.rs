c3b800d1b29b6522e8708307a6a76e0d08543138
Clément Boudereau <cboudereau@gmail.com>
2024-04-22 12:46:03 +0200

    2015/d6/rust: add part2

--- 2015/day6/rust/src/main.rs
+++ 2015/day6/rust/src/main.rs
@@ -52,11 +52,64 @@ fn part1(input: &str) -> i32 {
     counter
 }
 
+fn part2(input: &str) -> i32 {
+    let input = input.split("\r\n");
+    let regex = Regex::new(r"([A-Za-z\s]+)\s(\d+),(\d+)\sthrough\s(\d+),(\d+)").unwrap();
+    let mut matrix = vec![vec![0; 1000]; 1000];
+
+    for line in input {
+        for (_, [action, xmin, ymin, xmax, ymax]) in regex.captures_iter(line).map(|c| c.extract())
+        {
+            let action = match action {
+                "turn on" => Action::TurnOn,
+                "turn off" => Action::TurnOff,
+                "toggle" => Action::Toggle,
+                other => panic!("unexpected '{other}' line"),
+            };
+
+            let xmin = xmin.parse::<usize>().unwrap();
+            let ymin = ymin.parse::<usize>().unwrap();
+            let xmax = xmax.parse::<usize>().unwrap();
+            let ymax = ymax.parse::<usize>().unwrap();
+
+            // optimized by running cargo clippy --all-targets --all-features -- -D warnings
+            for i in ymin..=ymax {
+                for j in xmin..=xmax {
+                    let previous = matrix[i][j];
+                    matrix[i][j] = match action {
+                        Action::TurnOn => previous + 1,
+                        Action::TurnOff => {
+                            if previous > 0 {
+                                previous - 1
+                            } else {
+                                previous
+                            }
+                        }
+                        Action::Toggle => previous + 2,
+                    }
+                }
+            }
+        }
+    }
+
+    let mut counter = 0;
+    for row in matrix.into_iter() {
+        for cell in row {
+            counter += cell;
+        }
+    }
+
+    counter
+}
+
 fn main() {
     let input = include_str!("../../day6.txt");
 
     let part1 = part1(input);
-    println!("part1: {part1}")
+    println!("part1: {part1}");
+
+    let part2 = part2(input);
+    println!("part2: {part2}");
 }
 
 #[cfg(test)]
@@ -64,6 +117,12 @@ fn main() {
 fn test_part1() {
     let input = include_str!("../../day6.txt");
     assert_eq!(377891, part1(input))
+}
+
+#[test]
+fn test_part2() {
+    let input = include_str!("../../day6.txt");
+    assert_eq!(14110788, part2(input))
 }
 
 #[test]
