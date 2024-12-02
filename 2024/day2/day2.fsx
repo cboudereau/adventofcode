1b2f34bc2a8a58e96fd4dd3b4e4f3ca26cb2ed31
Clément Boudereau <cboudereau@gmail.com>
2024-12-02 12:43:53 +0100

    clean

--- 2024/day2/day2.fsx
+++ 2024/day2/day2.fsx
@@ -3,20 +3,19 @@ let readFile filePath =
     |> Array.map (fun x -> x.Split(' ') |> Array.map System.Int64.Parse)
 
 
-let part1 x = 
+let part1 count x = 
     let isSafe (a:int64[]) = 
-        // 7 6 4 2 1
         let rec isSafe isNegative (a:int64 list) = 
             match a, isNegative with
             | h1::h2::_, None -> isSafe (Some (h2 < h1)) a
-            | h1::h2::_, Some isNegative when let v = abs (h2 - h1) in v > 3 || v < 1 || (isNegative && v > 0) || (not isNegative && v < 0) -> false
+            | h1::h2::_, Some isNegative when let v = h2 - h1 in let va = abs (h2 - h1) in va > 3 || va < 1 || (isNegative && v > 0) || (not isNegative && v < 0) -> false
             | _::t, _ -> isSafe isNegative t
             | [], _ -> true
         a |> Array.toList |> isSafe None
     x 
     |> Array.filter isSafe
-    // |> Array.length
+    |> Array.length
 
 
-"sample.txt" |> readFile |> part1 //= 2
-"day2.txt" |> readFile |> part1
+"sample.txt" |> readFile |> part1 0 = 2
+"day2.txt" |> readFile |> part1 0 = 269
